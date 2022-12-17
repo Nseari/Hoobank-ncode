2b445a8c2719b9ad73340f8066eaa5f036f59882
Nseari Ndifon <onsearinahud@gmail.com>
2022-12-17 20:08:37 +0100

    updates cardDeal

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -17,10 +17,9 @@ const Billing = () => (
       <div className={layout.sectionInfo}>
         <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden" /> billing & invoicing</h2>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-          Elit enim sed mas sjjdjdjjd   jssjsjsj
-          hhdjdnjsnddksksksnsskdksskskskk jssjsjsj
-          shhshhshsj jjsjsjjs jjjsjsj jjjsjsjj
-          hssjsjjjjsjsjjjjsjsjjsj jdjdjjdjdjdjjdjdjj
+        With the ability to customize and automate invoices, you can save time and 
+        streamline your billing process. We offer integrations with popular payment gateways, 
+        making it easy for your clients  to pay you electronically.
         </p> 
 
         <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -10,8 +10,14 @@ const CardDeal = () => (
         steps.
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
-        aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+      Finding a better credit card deal can be done in a few simple steps. 
+      First, make a list of your credit card needs and preferences, such as the 
+      type of rewards you want, the annual fee you are willing to pay, and any specific
+      features you are looking for. Next, do some research to find credit card offers that meet 
+      your needs and compare the terms and conditions of each offer. Consider factors such as the
+      annual percentage rate (APR), fees, and any introductory offers or bonuses. Finally, read reviews
+      and compare the experiences of other customers before making a decision. 
+      By following these steps, you can find a credit card that better meets your needs and offers the best value.
       </p>
 
       <Button styles={`mt-10`} />
